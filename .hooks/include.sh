84f797208f7eee6721985e7c953dfde36fbc987c
Sekkemr <sekkmer@gmail.com>
2022-05-19 20:49:05 +0200

    [FIX] GitHub action no LFS

--- .hooks/include.sh
+++ .hooks/include.sh
@@ -5,12 +5,15 @@ function yarn_helper() {
 }
 
 function lfs_helper() {
+	if [ "${NO_GIT_LFS:-}" == "1" ]; then
+		return
+	fi
 	if command -v git-lfs >/dev/null 2>&1; then
 		local lsf_command="${1}"
 		shift
 		git lfs "${lsf_command}" "$@"
 	else
-		echo >&2 "\nThis repository is configured for Git LFS but 'git-lfs' was not found on your path."
+		echo "This repository is configured for Git LFS but 'git-lfs' was not found on your path." >&2
 		exit 2
 	fi
 }
