b3262948278a1e64d8fdf1b322e033e01d77de6c
jomshir98 <jomshir98@protonmail.com>
2022-05-21 14:57:33 +0200

    [ADD] Git LFS postinstall init

--- scripts/postinstall.cjs
+++ scripts/postinstall.cjs
@@ -3,13 +3,25 @@
 const { constants } = require('fs');
 const { copyFile } = require('fs/promises');
 const { resolve } = require('path');
-const { spawnSync } = require('child_process');
+const { spawnSync, execSync } = require('child_process');
+
+function commandExists(commandName) {
+	try {
+		const stdout = process.platform === 'win32' ?
+			execSync(`where ${commandName}`, { stdio: [] }) :
+			execSync(`command -v ${commandName} 2>/dev/null && { echo >&1 ${commandName}; exit 0; }`);
+		return !!stdout;
+	} catch (error) {
+		return false;
+	}
+}
 
 postinstall();
 
 async function postinstall() {
 	await copyDotenv();
 	configureGitHooks();
+	configureGitLFS();
 }
 
 async function copyDotenv() {
@@ -39,4 +51,39 @@ function configureGitHooks() {
 		throw error;
 
 	console.log('Git hooks path configured');
+}
+
+function configureGitLFS() {
+	if (process.env.NO_GIT_LFS)
+		return;
+
+	{
+		const { status, stdout } = spawnSync('git', ['config', '--local', '--get', 'filter.lfs.clean']);
+		if (status === 0 && stdout.toString().trim().length > 0)
+			return;
+	}
+
+	if (!commandExists('git-lfs')) {
+		console.error(`\n[ERROR] This repository is configured for Git LFS but 'git-lfs' was not found on your path.\n`);
+		process.exitCode = 2;
+		return;
+	}
+
+	console.log('Setting up Git LFS');
+	{
+		const { error, status, stderr } = spawnSync('git', ['lfs', 'install', '--local', '--manual']);
+		if (error)
+			throw error;
+		if (status !== 0) {
+			throw new Error(`Git LFS install failed:\n${stderr.toString().trim()}`);
+		}
+	}
+	{
+		const { error, status } = spawnSync('git', ['lfs', 'pull'], { stdio: 'inherit' });
+		if (error)
+			throw error;
+		if (status !== 0) {
+			throw new Error(`Git LFS pull failed`);
+		}
+	}
 }
