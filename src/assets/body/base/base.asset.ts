7a2a146f1f1306f570e187e771c344a320591f22
jomshir98 <jomshir98@protonmail.com>
2022-06-15 00:57:35 +0200

    [CHANGE] Move body to json file

--- src/assets/body/base/base.asset.ts
+++ src/assets/body/base/base.asset.ts
@@ -1,10 +1,6 @@
 import { DefineAsset } from '../../../tools';
-import { LoadAssetsGraphics } from '../../../tools/graphics';
-import { layers } from './base.layers';
-
-const graphics = LoadAssetsGraphics(layers);
 
 DefineAsset({
 	name: 'Base body',
-	graphics,
+	graphics: 'graphics.json',
 });
