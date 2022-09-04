19907b7a811c028db6f42b596a454fe78952d981
jomshir98 <jomshir98@protonmail.com>
2022-09-04 16:46:30 +0000

    [FEATURE] Item colorization

--- src/types.d.ts
+++ src/types.d.ts
@@ -7,7 +7,7 @@ type LayerImageOverrideCompressed = import('pandora-common').LayerImageOverrideC
 type LayerMirror = import('pandora-common').LayerMirror;
 type AllBones = import('./bones').AllBones;
 
-interface IntermediateAssetDefinition extends Pick<import('pandora-common').AssetDefinition, 'name' | 'actionMessages'> {
+interface IntermediateAssetDefinition extends Pick<import('pandora-common').AssetDefinition, 'name' | 'actionMessages' | 'colorization'> {
 	id?: string;
 	bodypart?: import('./bodyparts').BodypartName;
 	graphics?: string;
