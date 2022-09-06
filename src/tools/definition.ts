5388c4bc8a571d6c5763963cc0f80e10a54b4ed0
jomshir98 <jomshir98@protonmail.com>
2022-09-06 06:47:51 +0000

    [FEATURE] Asset pose limits

--- src/tools/definition.ts
+++ src/tools/definition.ts
@@ -1,27 +1,52 @@
-import { AssetDefinition, AssetId, GetLogger, HexColorStringSchema } from 'pandora-common';
+import { AssetDefinition, AssetId, BONE_MAX, BONE_MIN, GetLogger, HexColorStringSchema } from 'pandora-common';
 import { AssetDatabase } from './assetDatabase';
 import { AssetSourcePath, DefaultId } from './context';
 import { LoadAssetsGraphics } from './graphics';
 import { GraphicsDatabase } from './graphicsDatabase';
 import { join } from 'path';
 
-const logger = GetLogger('DefineAsset');
-
 export function DefineAsset(def: IntermediateAssetDefinition): void {
 	const id: AssetId = `a/${def.id ?? DefaultId()}` as const;
 
+	const logger = GetLogger('DefineAsset', `[Asset ${id}]`);
+
+	let definitionValid = true;
+
 	if (def.colorization) {
-		let colorValid = true;
 		for (let i = 0; i < def.colorization.length; i++) {
 			const valid = HexColorStringSchema.safeParse(def.colorization[i].default).success;
 			if (!valid) {
-				colorValid = false;
-				logger.error(`Asset ${id} invalid default in colorization[${i}]: '${def.colorization[i].default}' is not a valid color, use full hex format, like '#ffffff'`);
+				definitionValid = false;
+				logger.error(`Invalid default in colorization[${i}]: '${def.colorization[i].default}' is not a valid color, use full hex format, like '#ffffff'`);
 			}
 		}
-		if (!colorValid) {
-			throw new Error('Invalid color');
+	}
+
+	if (def.poseLimits) {
+		if (def.poseLimits.forcePose) {
+			for (const [bone, value] of Object.entries(def.poseLimits.forcePose)) {
+				if (value == null)
+					continue;
+				let limit = typeof value === "number" ? [value, value] : value;
+
+				if (!Number.isInteger(limit[0]) || limit[0] < BONE_MIN || limit[1] > BONE_MAX) {
+					logger.error(`Invalid min limit for poseLimits.forcePose.${bone}, must be a whole number in range [${BONE_MIN}, ${BONE_MAX}]`);
+					definitionValid = false;
+				}
+				if (!Number.isInteger(limit[1]) || limit[1] < BONE_MIN || limit[1] > BONE_MAX) {
+					logger.error(`Invalid max limit for poseLimits.forcePose.${bone}, must be a whole number in range [${BONE_MIN}, ${BONE_MAX}]`);
+					definitionValid = false;
+				}
+				if (limit[0] > limit[1]) {
+					logger.error(`Invalid range for poseLimits.forcePose.${bone}, min must not be greater than max`);
+					definitionValid = false;
+				}
+			}
 		}
+	}
+
+	if (!definitionValid) {
+		throw new Error('Invalid definition');
 	}
 
 	const asset: AssetDefinition = {
@@ -30,6 +55,7 @@ export function DefineAsset(def: IntermediateAssetDefinition): void {
 		actionMessages: def.actionMessages,
 		bodypart: def.bodypart,
 		colorization: def.colorization,
+		poseLimits: def.poseLimits,
 		hasGraphics: def.graphics !== undefined,
 	};
 
