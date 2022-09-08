144d133732f022626ff6005e62111522627cab01
jomshir98 <jomshir98@protonmail.com>
2022-09-08 12:21:13 +0000

    [ADD] Colorization for bra and t-shirt

--- src/assets/bras/style1/style1.asset.ts
+++ src/assets/bras/style1/style1.asset.ts
@@ -3,4 +3,10 @@ import { DefineAsset } from '../../../tools';
 DefineAsset({
 	name: 'Bra (style 1)',
 	graphics: 'graphics.json',
+	colorization: [
+		{
+			name: 'Bra',
+			default: '#FA5F55',
+		},
+	],
 });
--- src/assets/tops/t-shirt1/t-shirt1.asset.ts
+++ src/assets/tops/t-shirt1/t-shirt1.asset.ts
@@ -3,4 +3,14 @@ import { DefineAsset } from '../../../tools';
 DefineAsset({
 	name: 'T-Shirt',
 	graphics: 'graphics.json',
+	colorization: [
+		{
+			name: 'Shirt',
+			default: '#FFFFFF',
+		},
+		{
+			name: 'Print',
+			default: '#FFFF00',
+		},
+	],
 });
