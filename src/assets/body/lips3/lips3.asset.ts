27d585c0dffc2bca7360e8e9407e2f5cfce82548
jomshir98 <jomshir98@protonmail.com>
2022-09-09 22:25:12 +0000

    [ADD] Lips 3

---
+++ src/assets/body/lips3/lips3.asset.ts
@@ -0,0 +1,13 @@
+import { DefineAsset } from '../../../tools';
+
+DefineAsset({
+	name: 'Lips 3',
+	bodypart: 'lips',
+	graphics: 'graphics.json',
+	colorization: [
+		{
+			name: 'Lips',
+			default: '#F39989',
+		},
+	],
+});
