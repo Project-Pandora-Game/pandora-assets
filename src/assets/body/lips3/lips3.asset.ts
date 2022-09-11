5da6e6207316c00c98a49ce9af8bae3f661bf5b4
jomshir98 <jomshir98@protonmail.com>
2022-09-11 20:18:14 +0000

    [ADD] Lips3 expressions
    Co-authored-by: ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>

--- src/assets/body/lips3/lips3.asset.ts
+++ src/assets/body/lips3/lips3.asset.ts
@@ -1,3 +1,4 @@
+import { ItemInteractionType } from 'pandora-common';
 import { DefineAsset } from '../../../tools';
 
 DefineAsset({
@@ -10,4 +11,23 @@ DefineAsset({
 			default: '#F39989',
 		},
 	],
+	modules: {
+		mouth: {
+			type: 'typed',
+			name: 'Mouth expressions',
+			expression: 'Mouth',
+			interactionType: ItemInteractionType.ACCESS_ONLY,
+			variants: [
+				{
+					id: 'neutral',
+					name: 'Neutral',
+					default: true,
+				},
+				{
+					id: 'faint',
+					name: 'Faint Smile',
+				},
+			],
+		},
+	},
 });
