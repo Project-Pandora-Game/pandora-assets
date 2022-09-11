75b07714e76fac570c08648b1ee02671c243ad44
jomshir98 <jomshir98@protonmail.com>
2022-09-11 20:12:32 +0000

    [ADD] Leather Ankle Cuffs variants
    Co-authored-by: ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>

--- src/assets/foot_restraints/leather_ankle_cuffs/leather_ankle_cuffs.asset.ts
+++ src/assets/foot_restraints/leather_ankle_cuffs/leather_ankle_cuffs.asset.ts
@@ -1,3 +1,4 @@
+import { ItemInteractionType } from 'pandora-common';
 import { DefineAsset } from '../../../tools';
 
 DefineAsset({
@@ -21,12 +22,54 @@ DefineAsset({
 			default: '#FFFFFF',
 		},
 	],
-	poseLimits: {
-		forcePose: {
-			leg_r: 0,
-			leg_l: 0,
-			sitting: 0,
-			kneeling: 0,
+	modules: {
+		cuffState: {
+			type: 'typed',
+			name: 'Cuff states',
+			interactionType: ItemInteractionType.ACCESS_ONLY,
+			variants: [
+				{
+					id: 'unchained',
+					name: 'No Chain',
+					default: true,
+				},
+				{
+					id: 'normal',
+					name: 'Chained standing',
+					poseLimits: {
+						forcePose: {
+							leg_r: 0,
+							leg_l: 0,
+							sitting: 0,
+							kneeling: 0,
+						},
+					},
+				},
+				{
+					id: 'spread',
+					name: 'Chained Spread',
+					poseLimits: {
+						forcePose: {
+							leg_r: -3,
+							leg_l: -3,
+							sitting: 0,
+							kneeling: 0,
+						},
+					},
+				},
+				{
+					id: 'closed',
+					name: 'Chained Closed',
+					poseLimits: {
+						forcePose: {
+							leg_r: 2,
+							leg_l: 2,
+							sitting: 0,
+							kneeling: 0,
+						},
+					},
+				},
+			],
 		},
 	},
 	actionMessages: {
