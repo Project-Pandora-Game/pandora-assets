ed2d259b2523e311f9e1a3df4fbd46decbe15d23
jomshir98 <jomshir98@protonmail.com>
2022-09-11 00:11:41 +0000

    [ADD] T-Shirt selectable print

--- src/assets/tops/t-shirt1/t-shirt1.asset.ts
+++ src/assets/tops/t-shirt1/t-shirt1.asset.ts
@@ -13,4 +13,29 @@ DefineAsset({
 			default: '#FFFF00',
 		},
 	],
+	modules: {
+		prints: {
+			type: 'typed',
+			name: 'T-Shirt Print',
+			variants: [
+				{
+					id: 'kissingSmilie',
+					name: 'Kissing Smilie',
+					default: true,
+				},
+				{
+					id: 'smilie',
+					name: 'Smilie',
+				},
+				{
+					id: 'crown',
+					name: 'Crown',
+				},
+				{
+					id: 'noPrint',
+					name: 'No Print',
+				},
+			],
+		},
+	},
 });
