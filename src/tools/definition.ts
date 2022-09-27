01ad007d670ccd4afdc33ee7f378a1db789e021c
Jomshir98 <jomshir98@protonmail.com>
2022-09-27 16:14:36 +0000

    [DEV] Treat missing custom license as only alert, not warning

--- src/tools/definition.ts
+++ src/tools/definition.ts
@@ -73,7 +73,8 @@ export function GlobalDefineAsset(def: IntermediateAssetDefinition): void {
 		if (license.license.startsWith('./')) {
 			const path = join(AssetSourcePath, license.license);
 			if (!fs.existsSync(path) || !fs.statSync(path).isFile()) {
-				logger.warning(`Custom license '${license.license}' doesn't exist or is not a file.`);
+				// TODO: Change from `alert` to `warning` once ready
+				logger.alert(`Custom license '${license.license}' doesn't exist or is not a file.`);
 			}
 		}
 	}
