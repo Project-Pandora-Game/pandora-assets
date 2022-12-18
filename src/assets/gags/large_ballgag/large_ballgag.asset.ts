8fb9038f56450ae138509b671c83d22fae5574b6
ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>
2022-12-18 12:09:07 +0000

    [ADD] new large ball gag

---
+++ src/assets/gags/large_ballgag/large_ballgag.asset.ts
@@ -0,0 +1,60 @@
+DefineAsset({
+	name: 'Large Ball Gag',
+	size: 'medium',
+	graphics: 'graphics.json',
+	colorization: [
+		{
+			name: 'Ball',
+			default: '#C99AF8',
+		},
+		{
+			name: 'Straps',
+			default: '#444444',
+		},
+	],
+	attributes: [
+		'Restraint',
+		'Restraint_mouth',
+	],
+	requirements: [
+		'Mouth_open_wide',
+		'!Mouth_tongue_out',
+	],
+	modules: {
+		lock: {
+			type: 'lockSlot',
+			name: 'Lock',
+			lockRequirements: ['Lock'],
+			occupiedEffects: {
+				blockAddRemove: true,
+			},
+		},
+	},
+	effects: {
+		lipsTouch: 7,
+		jawMove: 10,
+		tongueRoof: 6,
+		mouthBreath: 3,
+		throatBreath: 2,
+		coherency: 4,
+		stimulus: 2,
+	},
+	chat: {
+		actionAdd: 'SOURCE_CHARACTER strapped a Large Ball Gag tightly between TARGET_CHARACTER_DYNAMIC_POSSESSIVE lips.',
+		actionRemove: 'SOURCE_CHARACTER loosened and then removed the Large Ball Gag from TARGET_CHARACTER_DYNAMIC_POSSESSIVE mouth.',
+	},
+	ownership: {
+		responsibleContributor: 'ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>',
+		credits: ['ClaudiaMia'],
+		modificationPolicy: `Fixes and New uses, otherwise ask`,
+		reusePolicy: 'Ask first',
+		licensing: [
+			{
+				source: 'Self-Made',
+				copyrightHolder: 'ClaudiaMia',
+				editedBy: 'ClaudiaMia',
+				license: 'Pandora-Use-Only',
+			},
+		],
+	},
+});
