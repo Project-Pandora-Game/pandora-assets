a33a60c3170f41f73372167eda3ec8ae34fe60fd
ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>
2022-12-18 11:20:05 +0000

    [ADD] new dildo gag

---
+++ src/assets/gags/dildogag/dildogag.asset.ts
@@ -0,0 +1,60 @@
+DefineAsset({
+	name: 'Dildo Gag',
+	size: 'medium',
+	graphics: 'graphics.json',
+	colorization: [
+		{
+			name: 'Gag',
+			default: '#6D6D6D',
+		},
+		{
+			name: 'Straps',
+			default: '#444444',
+		},
+	],
+	attributes: [
+		'Restraint',
+		'Restraint_mouth',
+	],
+	requirements: [
+		'Mouth_open_wide',
+		'!Mouth_tongue_out',
+	],
+	modules: {
+		lock: {
+			type: 'lockSlot',
+			name: 'Lock',
+			lockRequirements: ['Lock'],
+			occupiedEffects: {
+				blockAddRemove: true,
+			},
+		},
+	},
+	effects: {
+		lipsTouch: 9,
+		jawMove: 9,
+		tongueRoof: 8,
+		mouthBreath: 6,
+		throatBreath: 4,
+		coherency: 7,
+		stimulus: 6,
+	},
+	chat: {
+		actionAdd: 'SOURCE_CHARACTER pushed a Dildo Gag into TARGET_CHARACTER_DYNAMIC_POSSESSIVE mouth, strapping it tight.',
+		actionRemove: 'SOURCE_CHARACTER loosened and then removed the Dildo Gag from TARGET_CHARACTER_DYNAMIC_POSSESSIVE mouth.',
+	},
+	ownership: {
+		responsibleContributor: 'ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>',
+		credits: ['ClaudiaMia', 'Jomshir'],
+		modificationPolicy: `Fixes and New uses, otherwise ask`,
+		reusePolicy: 'Ask first',
+		licensing: [
+			{
+				source: 'Self-Made',
+				copyrightHolder: 'ClaudiaMia',
+				editedBy: 'ClaudiaMia',
+				license: 'Pandora-Use-Only',
+			},
+		],
+	},
+});
