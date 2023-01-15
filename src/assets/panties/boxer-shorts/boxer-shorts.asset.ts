45409144100f882f8359d0af823247b219e8ae61
Ayesha <ayeshafox44@gmail.com>
2023-01-15 14:43:55 -0600

    Boxer Shorts

---
+++ src/assets/panties/boxer-shorts/boxer-shorts.asset.ts
@@ -0,0 +1,37 @@
+DefineAsset({
+	name: 'Boxer Shorts',
+	size: 'small',
+	graphics: 'graphics.json',
+	colorization: [
+		{
+			name: 'Boxers',
+			default: '#FF8585',
+		},
+		{
+			name: 'Waistband',
+			default: '#FFFFFF',
+		},
+
+	],
+	attributes: [
+		'Underwear',
+		'Underwear_panties',
+	],
+	hides: [
+		'Penis',
+	],
+	ownership: {
+		responsibleContributor: 'Ayesha <ayeshafox44@gmail.com>',
+		credits: ['Ayesha'],
+		modificationPolicy: 'Ask first',
+		reusePolicy: 'Ask first',
+		licensing: [
+			{
+				source: 'Self-Made',
+				copyrightHolder: 'Ayesha',
+				editedBy: 'Ayesha',
+				license: 'Pandora-Use-Only',
+			},
+		],
+	},
+});
