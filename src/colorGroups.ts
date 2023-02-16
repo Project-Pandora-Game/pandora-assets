3ae27cb436d4912f7bb43a26420644ea86f86ccf
Sekkmer <sekkmer@gmail.com>
2023-02-16 01:37:25 +0100

    [CHANGE] color group for hair

--- src/colorGroups.ts
+++ src/colorGroups.ts
@@ -2,6 +2,8 @@ import { HexColorString, HexColorStringSchema } from 'pandora-common';
 
 export const COLOR_GROUP_DEFINITION = {
 	skin: '#FFECDF',
+	hair: '#555555',
+	hairShine: '#888888',
 } as const satisfies Readonly<Record<string, HexColorString>>;
 
 if (Object.values(COLOR_GROUP_DEFINITION).some((color) => !HexColorStringSchema.safeParse(color).success)) {
