f73adf6576febcf2e1c4f9f8c30be7bb8b3a1c3e
ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>
2023-03-25 06:51:07 +0000

    [ADD] latex dress

---
+++ src/assets/dresses/latex_dress/latex_dress.asset.ts
@@ -0,0 +1,70 @@
+DefineAsset({
+	name: 'Latex Dress',
+	size: 'medium',
+	allowRandomizerUsage: true,
+	graphics: 'graphics.json',
+	colorization: {
+		dress: {
+			name: 'Dress',
+			default: '#FFFFFF',
+		},
+	},
+	attributes: [
+		'Clothing',
+		'Clothing_upper',
+		'Clothing_lower',
+		'Clothing_large',
+	],
+	modules: {
+		skirtStateFront: {
+			type: 'typed',
+			name: 'Skirt State Front',
+			variants: [
+				{
+					id: 'normal',
+					name: 'Normal',
+					default: true,
+				},
+				{
+					id: 'up',
+					name: 'Pulled Up',
+				},
+			],
+		},
+		skirtStateBack: {
+			type: 'typed',
+			name: 'Skirt State Back',
+			variants: [
+				{
+					id: 'normal',
+					name: 'Normal',
+					default: true,
+				},
+				{
+					id: 'up',
+					name: 'Pulled up',
+				},
+			],
+		},
+	},
+	poseLimits: {
+		bones: {
+			arm_r: [[-75,120]],
+			arm_l: [[-75,120]],
+		},
+	},
+	ownership: {
+		responsibleContributor: 'ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>',
+		credits: ['ClaudiaMia'],
+		modificationPolicy: `Fixes and New uses, otherwise ask`,
+		reusePolicy: 'Ask first',
+		licensing: [
+			{
+				source: 'Self-Made',
+				copyrightHolder: 'ClaudiaMia',
+				editedBy: 'ClaudiaMia',
+				license: 'Pandora-Use-Only',
+			},
+		],
+	},
+});
