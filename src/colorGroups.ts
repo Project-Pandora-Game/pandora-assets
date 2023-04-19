5b6c5f87c656486c04ee3950814478f2af288d37
ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>
2023-04-19 18:40:47 +0000

    [CHANGE] affected asset changes

--- src/colorGroups.ts
+++ src/colorGroups.ts
@@ -2,8 +2,8 @@ import { HexRGBAColorString, HexRGBAColorStringSchema } from 'pandora-common';
 
 export const COLOR_GROUP_DEFINITION = {
 	skin: '#FFECDF',
-	hair: '#555555',
-	hairShine: '#888888',
+	hair: '#444444',
+	hairShine: '#FFFFFF55',
 } as const satisfies Readonly<Record<string, HexRGBAColorString>>;
 
 if (Object.values(COLOR_GROUP_DEFINITION).some((color) => !HexRGBAColorStringSchema.safeParse(color).success)) {
