bf3ace91da68a689b02b59c45f09ee1245bb29f3
Jomshir98 <jomshir98@protonmail.com>
2023-05-06 18:12:07 +0200

    [FIX] Update jest config type

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
-/*
+/**
  * For a detailed explanation regarding each configuration property and type check, visit:
  * https://jestjs.io/docs/configuration
+ * @type { import('ts-jest').JestConfigWithTsJest }
  */
 module.exports = {
 	testPathIgnorePatterns: ['/node_modules/', '<rootDir>/dist/', '<rootDir>/pandora/'],
