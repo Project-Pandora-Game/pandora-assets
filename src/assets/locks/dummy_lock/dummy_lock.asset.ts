6b44e7415d06af89be970b7703d3b17a3ef01ecf
Sekkmer <sekkmer@gmail.com>
2023-06-04 21:16:29 +0200

    [CHORE] cleanup

--- src/assets/locks/dummy_lock/dummy_lock.asset.ts
+++ src/assets/locks/dummy_lock/dummy_lock.asset.ts
@@ -5,33 +5,6 @@ DefineLockAsset({
 		actionLock: 'SOURCE_CHARACTER clicked the dummy lock on ITEM_CONTAINER_SIMPLE_DYNAMIC shut.',
 		actionUnlock: 'SOURCE_CHARACTER unlocked the dummy lock on ITEM_CONTAINER_SIMPLE_DYNAMIC.',
 	},
-	/*
-	modules: {
-		state: {
-			type: 'typed',
-			name: 'State',
-			variants: [
-				{
-					id: 'unlocked',
-					name: 'Unlocked',
-					default: true,
-					switchMessage: 'SOURCE_CHARACTER unlocked the dummy lock on ITEM_CONTAINER_SIMPLE_DYNAMIC.',
-				},
-				{
-					id: 'locked',
-					name: 'Locked',
-					blockAddRemove: true,
-					switchMessage: 'SOURCE_CHARACTER clicked the dummy lock on ITEM_CONTAINER_SIMPLE_DYNAMIC shut.',
-					storeTime: true,
-					storeCharacter: true,
-					customText: [
-						'Locked by CHARACTER at TIME',
-					],
-				},
-			],
-		},
-	},
-	*/
 	ownership: {
 		responsibleContributor: 'Jomshir98 <jomshir98@protonmail.com>',
 		credits: ['Jomshir'],
--- src/assets/locks/exclusive_lock/exclusive_lock.asset.ts
+++ src/assets/locks/exclusive_lock/exclusive_lock.asset.ts
@@ -8,34 +8,6 @@ DefineLockAsset({
 	locked: {
 		blockSelf: true,
 	},
-	/*
-	modules: {
-		state: {
-			type: 'typed',
-			name: 'State',
-			variants: [
-				{
-					id: 'unlocked',
-					name: 'Unlocked',
-					default: true,
-					switchMessage: 'SOURCE_CHARACTER unlocked the exclusive lock on ITEM_CONTAINER_SIMPLE_DYNAMIC.',
-				},
-				{
-					id: 'locked',
-					name: 'Locked',
-					blockAddRemove: true,
-					blockSelfModules: ['state'],
-					switchMessage: 'SOURCE_CHARACTER clicked the exclusive lock on ITEM_CONTAINER_SIMPLE_DYNAMIC shut.',
-					storeTime: true,
-					storeCharacter: true,
-					customText: [
-						'Locked by CHARACTER at TIME',
-					],
-				},
-			],
-		},
-	},
-	*/
 	ownership: {
 		responsibleContributor: 'Jomshir98 <jomshir98@protonmail.com>',
 		credits: ['Jomshir'],
