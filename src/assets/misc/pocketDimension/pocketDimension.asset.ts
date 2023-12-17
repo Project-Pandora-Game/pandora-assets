d9f098542fb2522393c31cbfaebef710f29afc23
ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>
2023-12-17 09:35:35 +0100

    [FIX] added missing license info

--- src/assets/misc/pocketDimension/pocketDimension.asset.ts
+++ src/assets/misc/pocketDimension/pocketDimension.asset.ts
@@ -20,6 +20,14 @@ DefineAsset({
 		credits: ['Jomshir'],
 		modificationPolicy: `Fixes and New uses, otherwise ask`,
 		reusePolicy: 'Free to use',
-		licensing: [],
+		licensing: [
+			{
+				part: 'Preview',
+				source: 'https://www.flickr.com/photos/51686021@N07/41138945550/',
+				editedBy: 'ClaudiaMia',
+				copyrightHolder: 'Burnt Pineapple Productions',
+				license: 'Public Domain',
+			},
+		],
 	},
 });
