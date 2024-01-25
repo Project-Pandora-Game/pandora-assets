061d9ba42d71cf2eb2cdd93663d36a7c03c42f47
ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>
2024-01-25 22:35:45 +0100

    [ADD] ear plugs

---
+++ src/assets/headwear/ear_plugs/ear_plugs.asset.ts
@@ -0,0 +1,27 @@
+DefineAsset({
+	name: 'Ear Plugs',
+	size: 'small',
+	graphics: undefined,
+	colorization: undefined,
+	preview: 'preview.png',
+	attributes: {
+		provides: [
+			'Restraint_ears',
+		],
+	},
+	ownership: {
+		responsibleContributor: 'ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>',
+		credits: ['ClaudiaMia'],
+		modificationPolicy: `Fixes and New uses, otherwise ask`,
+		reusePolicy: 'Ask first',
+		licensing: [
+			{
+				part: 'preview',
+				source: 'https://flic.kr/p/by2VHL',
+				copyrightHolder: 'Paul Hudson',
+				editedBy: 'ClaudiaMia',
+				license: 'CC BY',
+			},
+		],
+	},
+});
