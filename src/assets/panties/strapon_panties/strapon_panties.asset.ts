918bc7e06dbc0c7dda88989654a1cb1e77b1f2e7
ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>
2024-02-11 15:48:27 +0100

    [ADD] strapon panties

---
+++ src/assets/panties/strapon_panties/strapon_panties.asset.ts
@@ -0,0 +1,90 @@
+DefineAsset({
+	name: 'Strapon Panties',
+	size: 'medium',
+	graphics: 'graphics.json',
+	colorization: {
+		panties: {
+			name: 'Pantiess',
+			default: '#FFFFFF',
+		},
+		straponSmall: {
+			name: 'Small Strapon',
+			default: '#A051D3',
+		},
+		straponLarge: {
+			name: 'Large Strapon',
+			default: '#B895E6',
+		},
+		straponHuge: {
+			name: 'Huge Strapon',
+			default: '#E5506E',
+		},
+	},
+	// size:240, y:569, centered
+	preview: 'preview.png',
+	attributes: {
+		provides: [
+			'Clothing',
+			'Underwear',
+			'Underwear_panties',
+			'Toy',
+		],
+	},
+	modules: {
+		lock: {
+			type: 'lockSlot',
+			name: 'Lock',
+			occupiedProperties: {
+				blockAddRemove: true,
+			},
+		},
+		straponLock: {
+			type: 'lockSlot',
+			name: 'Strapon Lock',
+			occupiedProperties: {
+				blockModules: ['strapons'],
+			},
+		},
+		strapons: {
+			type: 'typed',
+			name: 'Strapons',
+			variants: [
+				{
+					id: 'none',
+					name: 'None',
+					default: true,
+				},
+				{
+					id: 'large',
+					name: 'Large Strapon',
+				},
+				{
+					id: 'small',
+					name: 'Small Strapon',
+				},
+				{
+					id: 'both',
+					name: 'Both',
+				},
+				{
+					id: 'huge',
+					name: 'Huge',
+				},
+			],
+		},
+	},
+	ownership: {
+		responsibleContributor: 'ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>',
+		credits: ['ClaudiaMia'],
+		modificationPolicy: `Fixes and New uses, otherwise ask`,
+		reusePolicy: 'Ask first',
+		licensing: [
+			{
+				source: 'Self-Made',
+				copyrightHolder: 'ClaudiaMia',
+				editedBy: 'ClaudiaMia',
+				license: 'Pandora-Use-Only',
+			},
+		],
+	},
+});
