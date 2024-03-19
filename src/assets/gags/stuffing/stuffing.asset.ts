069393a3ee8cc95296c7a0cc6435dd8a94abc235
ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>
2024-03-19 22:00:37 +0100

    [ADD] stuffing asset

---
+++ src/assets/gags/stuffing/stuffing.asset.ts
@@ -0,0 +1,52 @@
+DefineAsset({
+	name: 'Stuffing',
+	size: 'small',
+	graphics: 'graphics.json',
+	colorization: {
+		cloth: {
+			name: 'Stuffing',
+			default: '#FDF3EA',
+		},
+	},
+	// size:200, y:197, centered
+	preview: 'preview.png',
+	attributes: {
+		provides: [
+			'Restraint',
+			'Restraint_mouth',
+			'Mouth_item',
+		],
+		requires: [
+			'Mouth_open_wide',
+			'!Mouth_protruding',
+			'!Mouth_cover',
+		],
+	},
+	effects: {
+		lipsTouch: 2,
+		jawMove: 3,
+		tongueRoof: 3,
+		mouthBreath: 4,
+		throatBreath: 2,
+		coherency: 2,
+		stimulus: 1,
+	},
+	chat: {
+		actionAdd: 'SOURCE_CHARACTER stuffed TARGET_CHARACTER_DYNAMIC_POSSESSIVE mouth.',
+		actionRemove: 'SOURCE_CHARACTER removed the stuffing in TARGET_CHARACTER_DYNAMIC_POSSESSIVE mouth.',
+	},
+	ownership: {
+		responsibleContributor: 'ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>',
+		credits: ['ClaudiaMia'],
+		modificationPolicy: `Fixes and New uses, otherwise ask`,
+		reusePolicy: 'Ask first',
+		licensing: [
+			{
+				source: 'Self-Made',
+				copyrightHolder: 'ClaudiaMia',
+				editedBy: 'ClaudiaMia',
+				license: 'Pandora-Use-Only',
+			},
+		],
+	},
+});
