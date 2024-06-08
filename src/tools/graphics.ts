f6ebfaf9d36a2da860591e473417f30a3da7b5f4
jomshir98 <jomshir98@protonmail.com>
2024-06-08 10:27:22 +0000

    [ADD] Validate against duplicate point definitions

--- src/tools/graphics.ts
+++ src/tools/graphics.ts
@@ -1,3 +1,7 @@
+import { readFileSync } from 'fs';
+import { Immutable } from 'immer';
+import { diffString } from 'json-diff';
+import { isEqual } from 'lodash';
 import {
 	AssetGraphicsDefinition,
 	AssetGraphicsDefinitionSchema,
@@ -5,18 +9,24 @@ import {
 	LayerDefinition,
 	LayerImageOverride,
 	LayerImageSetting,
+	Logger,
 	ModuleNameSchema,
+	PointTemplate,
+	PointTemplateDiff,
 	SCHEME_OVERRIDE,
 } from 'pandora-common';
-import { DefineImageResource } from './resources';
-import { readFileSync } from 'fs';
-import { GraphicsDatabase } from './graphicsDatabase';
-import { WatchFile } from './watch';
+import { relative } from 'path';
 import { z } from 'zod';
+import { SRC_DIR } from '../constants';
+import { GraphicsDatabase } from './graphicsDatabase';
+import { DefineImageResource } from './resources';
+import { WatchFile } from './watch';
 
 export const GENERATED_RESOLUTIONS: readonly number[] = [0.5, 0.25];
 
 export function LoadAssetsGraphics(path: string, assetModules: readonly string[]): AssetGraphicsDefinition {
+	const logger = GetLogger('GraphicsValidation').prefixMessages(`Graphics definition '${relative(SRC_DIR, path)}':\n\t`);
+
 	WatchFile(path);
 
 	const definition = JSON.parse(
@@ -38,16 +48,51 @@ export function LoadAssetsGraphics(path: string, assetModules: readonly string[]
 	const parseResult = AssetGraphicsDefinitionSchema.safeParse(definition);
 
 	if (!parseResult.success) {
-		GetLogger('GraphicsValidation').error(
-			`Graphics in '${path}' are not AssetGraphicsDefinition:\n`,
+		logger.error(
+			`Not AssetGraphicsDefinition:\n`,
 			parseResult.error.toString(),
 		);
 		throw new Error(`Graphics in '${path}' are not AssetGraphicsDefinition`);
+	} else if (!isEqual(definition, parseResult.data)) {
+		const diff = diffString(definition, parseResult.data, { color: false });
+		logger.warning('Definition updated during parse:\n', diff);
 	}
 
+	AssetGraphicsValidate(parseResult.data, logger);
+
 	return {
-		layers: definition.layers.map(LoadAssetLayer),
+		layers: parseResult.data.layers.map(LoadAssetLayer),
 	};
+}
+
+function AssetGraphicsValidate(definition: AssetGraphicsDefinition, logger: Logger): void {
+	for (let layerIndex = 0; layerIndex < definition.layers.length; layerIndex++) {
+		const layer = definition.layers[layerIndex];
+
+		const layerLogger = logger.prefixMessages(`Layer #${layerIndex} (${layer.name ?? '[UNNAMED]'}): `);
+
+		// If layer has custom points, validate them
+		if (Array.isArray(layer.points)) {
+			// Try to find if there isn't a point template matching the
+			for (const [template, templateData] of GraphicsDatabase.templates) {
+				if (PointDefinitionsMatch(templateData, layer.points)) {
+					layerLogger.warning(`Custom point definition exactly matches template '${template}'. Use template reference instead.`);
+				}
+			}
+			// Try to find if any earlier layer has a matching template
+			for (let otherLayerIndex = 0; otherLayerIndex < layerIndex; otherLayerIndex++) {
+				const otherLayer = definition.layers[otherLayerIndex];
+				if (Array.isArray(otherLayer.points) && PointDefinitionsMatch(otherLayer.points, layer.points)) {
+					layerLogger.warning(`Custom point definition exactly matches layer #${otherLayerIndex} (${otherLayer.name ?? '[UNNAMED]'}). Use reference for one of the layers instead.`);
+				}
+			}
+		}
+	}
+}
+
+function PointDefinitionsMatch(a: Immutable<PointTemplate>, b: Immutable<PointTemplate>): boolean {
+	const delta = PointTemplateDiff(a, b);
+	return delta.added == null && delta.removed == null;
 }
 
 function LoadLayerImage(image: string): string {
