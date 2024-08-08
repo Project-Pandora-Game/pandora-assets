41ad18c6b57b7cde95b07a93e0d11bf27677cfbc
ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>
2024-08-08 21:27:07 +0200

    [CHANGE] improve coloring of body and nose1

--- src/colorGroups.ts
+++ src/colorGroups.ts
@@ -1,7 +1,7 @@
 import { HexRGBAColorString, HexRGBAColorStringSchema } from 'pandora-common';
 
 export const COLOR_GROUP_DEFINITION = {
-	skin: '#FAF2EE',
+	skin: '#ECD5CC',
 	hair: '#444444',
 	hairShine: '#FFFFFF55',
 } as const satisfies Readonly<Record<string, HexRGBAColorString>>;
