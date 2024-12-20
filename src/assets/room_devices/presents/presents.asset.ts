b23b2e623421135877134ce26f049008139ffe48
Sandrine <118102950+SandrinePDR@users.noreply.github.com>
2024-12-20 14:54:47 +0100

    [ADD] Present (#324)

--- src/assets/room_devices/presents/presents.asset.ts
+++ src/assets/room_devices/presents/presents.asset.ts
@@ -1,20 +1,6 @@
 DefineRoomDeviceAsset({
 	name: 'Christmas Presents',
 	size: 'huge',
-	colorization: {
-		color_1: {
-			name: 'Color 1',
-			default: '#FFFFFF',
-		},
-		color_2: {
-			name: 'Color 2',
-			default: '#33922C',
-		},
-		color_3: {
-			name: 'Color 3',
-			default: '#EF5151',
-		},
-	},
 	staticAttributes: ['Floor'],
 	preview: 'preview.png',
 	slots: {},
@@ -28,24 +14,13 @@ DefineRoomDeviceAsset({
 		},
 	},
 	pivot: {
-		x: 660,
+		x: 350,
 		y: 400,
 	},
 	graphicsLayers: [
 		{
 			type: 'sprite',
-			image: 'presents_color_1.png',
-			colorizationKey: 'color_1',
-		},
-		{
-			type: 'sprite',
-			image: 'presents_color_2.png',
-			colorizationKey: 'color_2',
-		},
-		{
-			type: 'sprite',
-			image: 'presents_color_3.png',
-			colorizationKey: 'color_3',
+			image: 'presents.png',
 		},
 	],
 	ownership: {
