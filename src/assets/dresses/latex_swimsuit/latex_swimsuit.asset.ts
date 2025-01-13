2d65ab47f4fe9c6a8deca4ac2e5df31cf529842f
SandrinePDR <118102950+SandrinePDR@users.noreply.github.com>
2025-01-13 20:45:36 +0100

    [ADD] Latex body

---
+++ src/assets/dresses/latex_swimsuit/latex_swimsuit.asset.ts
@@ -0,0 +1,52 @@
+DefineAsset({
+	name: 'Latex Body',
+	size: 'small',
+	allowRandomizerUsage: true,
+	graphics: 'graphics.json',
+	colorization: {
+		body: {
+			name: 'Body',
+			default: '#020202FF',
+			minAlpha: 0.15,
+		},
+		shine: {
+			name: 'Reflection',
+			default: '#FFFFFF',
+		},
+	},
+	// size:420, y:350, centered
+	preview: 'preview.png',
+	attributes: {
+		provides: [
+			'Clothing',
+			'Clothing_upper',
+			'Clothing_lower',
+			'Anus_cover',
+			'Vulva_cover',
+		],
+	},
+	modules: {
+		lock: {
+			type: 'lockSlot',
+			name: 'Lock',
+			occupiedProperties: {
+				blockAddRemove: true,
+			},
+		},
+	},
+	ownership: {
+		responsibleContributor: 'Sandrine <118102950+SandrinePDR@users.noreply.github.com>',
+		credits: ['Taja'],
+		modificationPolicy: `Fixes and New uses, otherwise ask`,
+		reusePolicy: 'Ask first',
+		licensing: [
+			{
+				part: 'body',
+				source: 'Self-Made',
+				copyrightHolder: 'Taja',
+				editedBy: 'Sandrine',
+				license: 'Pandora-Use-Only-v1-or-later',
+			},
+		],
+	},
+});
