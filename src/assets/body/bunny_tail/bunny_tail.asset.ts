37d232c1032ca14c00689c2cbd682fdc6ef21adf
SandrinePDR <118102950+SandrinePDR@users.noreply.github.com>
2025-02-03 16:16:21 +0100

    [ADD] Bunny tail

---
+++ src/assets/body/bunny_tail/bunny_tail.asset.ts
@@ -0,0 +1,32 @@
+DefineBodypart({
+	name: 'Bunny Tail',
+	bodypart: 'tails',
+	allowRandomizerUsage: false,
+	graphics: 'graphics.json',
+	colorization: {
+		tail: {
+			name: 'Tail',
+			default: '#DDDDDD',
+		},
+	},
+	preview: null,
+	attributes: {
+		provides: [
+			'Tail',
+		],
+	},
+	ownership: {
+		responsibleContributor: 'Sandrine <118102950+SandrinePDR@users.noreply.github.com>',
+		credits: ['Taja'],
+		modificationPolicy: `Fixes and New uses, otherwise ask`,
+		reusePolicy: 'Ask first',
+		licensing: [
+			{
+				source: 'Self-Made',
+				copyrightHolder: 'Taja',
+				editedBy: 'Sandrine',
+				license: 'Pandora-Use-Only-v1-or-later',
+			},
+		],
+	},
+});
