eae0e8dc506bcaf5ddd14271f2d7a9de79fb2cee
ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>
2025-03-10 22:51:37 +0100

    [FIX] licensing of the lock items

--- src/assets/locks/timer_lock_12h/timer_lock_12h.asset.ts
+++ src/assets/locks/timer_lock_12h/timer_lock_12h.asset.ts
@@ -16,13 +16,6 @@ DefineLockAsset({
 		credits: ['ClaudiaMia'],
 		modificationPolicy: `Fixes and New uses, otherwise ask`,
 		reusePolicy: 'Ask first',
-		licensing: [
-			{
-				source: 'Self-Made',
-				copyrightHolder: 'ClaudiaMia',
-				editedBy: 'ClaudiaMia',
-				license: 'Pandora-Use-Only-v1-or-later',
-			},
-		],
+		licensing: [],
 	},
 });
