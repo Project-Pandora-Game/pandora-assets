30142830ec409cbed972e81cbb107d87ca925740
SandrinePDR <118102950+SandrinePDR@users.noreply.github.com>
2025-06-26 17:59:02 +0200

    [ADD] Graphics

---
+++ src/assets/arm_restraints/handcuffs/handcuffs.asset.ts
@@ -0,0 +1,157 @@
+DefineAsset({
+	name: 'Handcuffs',
+	size: 'small',
+	requireFreeHandsToUseDefault: true,
+	graphics: 'graphics.json',
+	colorization: {
+		cuff: {
+			name: 'Cuff',
+			default: '#FFFFFF',
+		},
+	},
+	// size:320, y:549, centered
+	preview: 'preview.png',
+	attributes: {
+		provides: [
+			'Restraint',
+			'Restraint_arms',
+			'Wrist_cuffs',
+		],
+	},
+	modules: {
+		cuffState: {
+			type: 'typed',
+			name: 'Cuff states',
+			variants: [
+				{
+					id: 'unchained',
+					name: 'No Chain',
+					default: true,
+				},
+				{
+					id: 'chained_front',
+					name: 'Chained in front',
+					properties: {
+						poseLimits: {
+							options: [
+								{
+									arms: {
+										position: 'front',
+										rotation: 'down',
+									},
+									bones: {
+										arm_r: 74,
+										arm_l: 74,
+										elbow_r: 43,
+										elbow_l: 43,
+									},
+								},
+								{
+									arms: {
+										rotation: 'up',
+									},
+									bones: {
+										arm_r: -74,
+										arm_l: -74,
+										elbow_r: -46,
+										elbow_l: -47,
+									},
+									options: [
+										{ arms: { position: 'front_above_hair' } },
+										{ arms: { position: 'front' } },
+										{ arms: { position: 'back' } },
+										{ arms: { position: 'back_below_hair' } },
+									],
+								},
+							],
+						},
+						stateFlags: {
+							provides: ['chain'],
+						},
+					},
+				},
+				{
+					id: 'chained_back',
+					name: 'Chained behind',
+					properties: {
+						poseLimits: {
+							options: [
+								{
+									arms: {
+										rotation: 'down',
+									},
+									bones: {
+										arm_r: 74,
+										arm_l: 74,
+										elbow_r: 43,
+										elbow_l: 43,
+									},
+									options: [
+										{ arms: { position: 'back' } },
+										{ arms: { position: 'back_below_hair' } },
+									],
+								},
+								{
+									bones: {
+										arm_r: 104,
+										arm_l: 104,
+										elbow_r: -4,
+										elbow_l: -4,
+									},
+									options: [
+										{ arms: { position: 'back' } },
+										{ arms: { position: 'back_below_hair' } },
+									],
+								},
+							],
+						},
+						stateFlags: {
+							provides: ['chain'],
+						},
+					},
+				},
+				{
+					id: 'reverse_prayer',
+					name: 'Reverse prayer',
+					properties: {
+						effects: {
+							blockHands: true,
+						},
+						poseLimits: {
+							arms: {
+								position: 'back',
+							},
+							bones: {
+								arm_r: 100,
+								arm_l: 100,
+								elbow_r: 130,
+								elbow_l: 130,
+							},
+						},
+						stateFlags: {
+							provides: ['chain'],
+						},
+					},
+				},
+			],
+		},
+	},
+	chat: {
+		actionAdd: 'SOURCE_CHARACTER fastened ITEM_ASSET_NAME around TARGET_CHARACTER_DYNAMIC_POSSESSIVE wrists.',
+		actionRemove: 'SOURCE_CHARACTER opened and slipped off ITEM_ASSET_NAME from TARGET_CHARACTER_DYNAMIC_POSSESSIVE wrists.',
+	},
+	ownership: {
+		responsibleContributor: 'Sandrine <118102950+SandrinePDR@users.noreply.github.com>',
+		credits: ['SandrinePDR'],
+		modificationPolicy: `Fixes and New uses, otherwise ask`,
+		reusePolicy: 'Ask first',
+		licensing: [
+			{
+				source: 'Self-Made',
+				copyrightHolder: 'ClaudiaMia',
+				editedBy: 'SandrinePDR',
+				license: 'Pandora-Use-Only-v1-or-later',
+			},
+		],
+	},
+});
