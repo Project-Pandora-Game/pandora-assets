71f4028773424acc302013350126598e5d2b607b
ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>
2025-09-09 12:30:26 +0200

    [ADD] block movement effect to the arm jute rope's hogtied state

--- src/assets/arm_restraints/arm_jute_ropes/arm_jute_ropes.asset.ts
+++ src/assets/arm_restraints/arm_jute_ropes/arm_jute_ropes.asset.ts
@@ -333,6 +333,9 @@ DefineAsset({
 								hogtie_anchor_point: 'Legs cannot be hogtied without a knot on back or wrist tied back',
 							},
 						},
+						effects: {
+							blockRoomMovement: true,
+						},
 					},
 				},
 			],
