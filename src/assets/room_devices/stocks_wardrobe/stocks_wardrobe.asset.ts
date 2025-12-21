8652f2689df2d336735f2484bf25da6410430fa8
ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>
2025-12-21 17:39:00 +0100

    [ADD] more storage module options to several room devices

--- src/assets/room_devices/stocks_wardrobe/stocks_wardrobe.asset.ts
+++ src/assets/room_devices/stocks_wardrobe/stocks_wardrobe.asset.ts
@@ -57,7 +57,7 @@ DefineRoomDeviceAsset({
 					name: 'Closed',
 					properties: {
 						blockSlotsEnterLeave: ['character_slot'],
-						blockModules: ['position'],
+						blockModules: ['position', 'storage'],
 					},
 				},
 				{
@@ -120,7 +120,15 @@ DefineRoomDeviceAsset({
 				blockModules: ['position'],
 			},
 		},
+		storage: {
+			type: 'storage',
+			name: `Wardrobe contents`,
+			staticConfig: { slotName: null },
+			maxAcceptedSize: 'large',
+			maxCount: 20,
+		},
 	},
+	storageModule: 'storage',
 	pivot: {
 		x: 500,
 		y: 1300,
