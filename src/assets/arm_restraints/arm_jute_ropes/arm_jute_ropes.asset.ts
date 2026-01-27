d7172add22098e4108532a9ff7755693bc9b8bdf
SandrinePDR <118102950+SandrinePDR@users.noreply.github.com>
2026-01-27 09:15:24 +0100

    [FIX] Typo on requirement description

--- src/assets/arm_restraints/arm_jute_ropes/arm_jute_ropes.asset.ts
+++ src/assets/arm_restraints/arm_jute_ropes/arm_jute_ropes.asset.ts
@@ -341,7 +341,7 @@ DefineAsset({
 						},
 						stateFlags: {
 							requires: {
-								hogtie_anchor_point: 'Legs cannot be hogtied without a knot on back or wrist tied back',
+								hogtie_anchor_point: 'Legs cannot be hogtied without a knot on the back or wrists tied back',
 							},
 						},
 						effects: {
