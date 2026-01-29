15ad4d01a4ffdfc9155277cd50800b78e397d0e1
Jomshir98 <jomshir98@protonmail.com>
2026-01-29 20:50:13 +0000

    [CHANGE] Update footwear room deployment offsets

--- src/assets/footwear/leather_shoes/leather_shoes.asset.ts
+++ src/assets/footwear/leather_shoes/leather_shoes.asset.ts
@@ -14,7 +14,7 @@ DefineAsset({
 		},
 	},
 	roomDeployment: {
-		autoDeployRelativePosition: [330, -100, 0],
+		autoDeployRelativePosition: [250, -1, 0],
 	},
 	// size:240, y:1143, centered
 	preview: 'preview.png',
