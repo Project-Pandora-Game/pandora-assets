8f2b64f1ca984d86efe77d6645203dc6d1762592
Shikifet <shikifet@gmail.com>
2026-02-23 07:02:04 +0100

    Create Asexual body sex

---
+++ src/assets/body/asexual_sex1/asexual_sex1.asset.ts
@@ -0,0 +1,28 @@
+DefineBodypart({
+	name: 'Asexual Sex 1',
+	bodypart: 'sex',
+	useForTesting: true,
+	allowRandomizerUsage: false,
+	graphics: 'graphics.json',
+	colorization: {
+	},
+	preview: null,
+	attributes: {
+	},
+	modules: {
+	},
+	ownership: {
+		responsibleContributor: 'Shikifet <shikifet@gmail.com>',
+		credits: ['Shikifet'],
+		modificationPolicy: `Fixes and New uses, otherwise ask`,
+		reusePolicy: 'Ask first',
+		licensing: [
+			{
+				source: 'Self-Made',
+				copyrightHolder: 'Shikifet',
+				editedBy: 'Shikifet',
+				license: 'Pandora-Use-Only-v1-or-later',
+			},
+		],
+	},
+});
