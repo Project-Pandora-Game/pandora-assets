09348261da7083251b461eb91f4540c2f4ab4c10
freyja-veritas <247626786+freyja-veritas@users.noreply.github.com>
2026-04-08 17:51:20 +0300

    added drop armhole tank top

---
+++ src/assets/tops/drop_armohole_tank-top/drop_armohole_tank-top.asset.ts
@@ -0,0 +1,52 @@
+DefineAsset({
+	name: 'Drop Armhole Tank Top',
+	size: 'medium',
+	allowRandomizerUsage: true,
+	graphics: 'graphics.json',
+	colorization: {
+		drop_armohole_tank_top: {
+			name: 'Drop Armhole Tank Top',
+			default: '#D4D4D4',
+		},
+	},
+	roomDeployment: {
+		autoDeployRelativePosition: [-90, -125, 0],
+	},
+	// size:350, y:380, centered
+	preview: 'preview.png',
+	attributes: {
+		provides: [
+			'Clothing',
+			'Clothing_upper',
+		],
+	},
+	modules: {
+		type: {
+			type: 'typed',
+			name: 'Drop Armhole Tank Top Type',
+			variants: [
+				{
+					id: 'regular',
+					name: 'Regular',
+					default: true,
+				},
+			],
+		},
+	},
+	ownership: {
+		responsibleContributor: 'Freyja <247626786+freyja-veritas@users.noreply.github.com>',
+		credits: ['Freyja'],
+		modificationPolicy: `Fixes and New uses, otherwise ask`,
+		reusePolicy: 'Ask first',
+		licensing: [
+			{
+				part: 'drop armhole tank top',
+				source: 'Self-Made',
+				copyrightHolder: 'Freyja',
+				editedBy: 'Freyja',
+				license: 'Pandora-Use-Only-v1-or-later',
+			},
+		],
+	},
+});
+
