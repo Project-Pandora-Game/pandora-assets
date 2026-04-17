3a72b62a02e5ae7a01236ee60d26d36d317ea89f
ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>
2026-04-17 18:19:25 +0200

    [ADD] new handheld asset remote control

---
+++ src/assets/handhelds/remote_control/remote_control.asset.ts
@@ -0,0 +1,102 @@
+DefineAsset({
+	name: 'Remote Control',
+	size: 'small',
+	graphics: 'graphics.json',
+	colorization: {
+		remote: {
+			name: 'Remote Control',
+			default: '#FF8EE5',
+		},
+	},
+	roomDeployment: {
+		autoDeployRelativePosition: [-150, -20, 0],
+	},
+	// size:200, y:317, X:5
+	preview: 'preview.png',
+	attributes: {
+		provides: [
+			'Handheld',
+		],
+		requires: [
+			'!Hand_restricting_cover',
+		],
+	},
+	modules: {
+		handUsage_r: {
+			type: 'typed',
+			name: 'Held in right hand',
+			variants: [
+				{
+					id: 'yes',
+					name: 'Yes',
+					default: true,
+					properties: {
+						poseLimits: {
+							rightArm: {
+								fingers: 'fist',
+								rotation: ['up', 'down'],
+							},
+						},
+					},
+				},
+				{
+					id: 'no',
+					name: 'No',
+				},
+			],
+		},
+		handUsage_l: {
+			type: 'typed',
+			name: 'Held in left hand',
+			variants: [
+				{
+					id: 'yes',
+					name: 'Yes',
+					properties: {
+						poseLimits: {
+							leftArm: {
+								fingers: 'fist',
+							},
+							options: [
+								{
+									leftArm: {
+										rotation: 'up',
+									},
+								},
+								{
+									leftArm: {
+										rotation: 'down',
+									},
+								},
+							],
+						},
+					},
+				},
+				{
+					id: 'no',
+					name: 'No',
+					default: true,
+
+				},
+			],
+		},
+	},
+	chat: {
+		actionAdd: 'SOURCE_CHARACTER put ITEM_ASSET_NAME into TARGET_CHARACTER_DYNAMIC_POSSESSIVE hand.',
+		actionRemove: 'SOURCE_CHARACTER removed ITEM_ASSET_NAME from TARGET_CHARACTER_DYNAMIC_POSSESSIVE hand.',
+	},
+	ownership: {
+		responsibleContributor: 'ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>',
+		credits: ['ClaudiaMia'],
+		modificationPolicy: `Fixes and New uses, otherwise ask`,
+		reusePolicy: 'Ask first',
+		licensing: [
+			{
+				source: 'Self-Made',
+				copyrightHolder: 'ClaudiaMia',
+				editedBy: 'ClaudiaMia',
+				license: 'Pandora-Use-Only-v1-or-later',
+			},
+		],
+	},
+});
