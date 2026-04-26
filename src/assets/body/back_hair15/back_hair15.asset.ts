96257e4cae1b3f9f7817af6a0dbe6a30725c8e4a
ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>
2026-04-26 12:42:53 +0200

    [ADD] front and back hairs 15

---
+++ src/assets/body/back_hair15/back_hair15.asset.ts
@@ -0,0 +1,58 @@
+import { CreateHairColor } from '../../../helpers/hair_base.ts';
+const { colorization } = CreateHairColor(false);
+
+const bodypart = DefineBodypart({
+	name: 'Back Hair 15',
+	bodypart: 'backhair',
+	allowRandomizerUsage: true,
+	graphics: 'graphics.json',
+	colorization,
+	// size:335, y:185, centered
+	preview: 'preview.png',
+	attributes: {
+		provides: [
+			'Hair',
+			'Hair_back',
+		],
+	},
+	ownership: {
+		responsibleContributor: 'ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>',
+		credits: ['ClaudiaMia'],
+		modificationPolicy: `Fixes and New uses, otherwise ask`,
+		reusePolicy: 'Ask first',
+		licensing: [
+			{
+				part: 'image',
+				source: 'Self-Made',
+				copyrightHolder: 'ClaudiaMia',
+				editedBy: 'ClaudiaMia',
+				license: 'Pandora-Use-Only-v1-or-later',
+			},
+			{
+				part: 'used 3D model',
+				source: 'https://skfb.ly/py8P7',
+				copyrightHolder: 'Ace-Jane',
+				editedBy: 'ClaudiaMia',
+				license: 'CC BY',
+			},
+		],
+	},
+});
+
+DefineAsset({
+	...bodypart,
+	id: 'body/back_hair15/wig',
+	name: 'Back Wig 15',
+	allowRandomizerUsage: undefined,
+	size: 'small',
+	attributes: {
+		provides: [
+			'Wig',
+			'Wig_back',
+		],
+		hides: [
+			'Hair_back',
+		],
+	},
+});
+
