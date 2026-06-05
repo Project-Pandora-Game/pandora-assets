1ff9e2af8b5b82492af28130b2ea1c6566fcf340
SandrinePDR <118102950+SandrinePDR@users.noreply.github.com>
2026-06-05 20:06:10 +0200

    [ADD] Hand pump

---
+++ src/assets/handhelds/hand_pump/hand_pump.asset.ts
@@ -0,0 +1,171 @@
+DefineAsset({
+	name: 'Hand Pump',
+	size: 'small',
+	graphics: 'graphics.json',
+	colorization: {
+		handle: {
+			name: 'Handle',
+			default: '#C191D6',
+		},
+		junction: {
+			name: 'Junction',
+			default: '#C191D6',
+		},
+		topLid: {
+			name: 'Top Lid',
+			default: '#C191D6',
+		},
+		glass: {
+			name: 'Glass',
+			default: '#1BB6FF00',
+			minAlpha: 0.8,
+		},
+		bottomLid: {
+			name: 'Bottom Lid',
+			default: '#C191D6',
+		},
+		suctionCup: {
+			name: 'Suction Cup',
+			default: '#FFB570',
+			minAlpha: 0.2,
+		},
+		fluid: {
+			name: 'Fluid',
+			default: '#FFE599',
+		},
+	},
+	roomDeployment: {
+		autoDeployRelativePosition: [350, -50, 0],
+	},
+	// size:200, y:475, X:500
+	preview: null, // 'preview.png',
+	attributes: {
+		provides: [
+			'Handheld',
+		],
+		requires: [
+			'!Hand_restricting_cover',
+		],
+	},
+	modules: {
+		handUsage_r: {
+			type: 'typed',
+			name: 'Held in right hand',
+			variants: [
+				{
+					id: 'yes',
+					name: 'Yes',
+					default: true,
+					properties: {
+						poseLimits: {
+							rightArm: {
+								fingers: 'fist',
+								rotation: 'down',
+							},
+						},
+					},
+				},
+				{
+					id: 'no',
+					name: 'No',
+				},
+			],
+		},
+		handUsage_l: {
+			type: 'typed',
+			name: 'Held in left hand',
+			variants: [
+				{
+					id: 'yes',
+					name: 'Yes',
+					properties: {
+						poseLimits: {
+							leftArm: {
+								fingers: 'fist',
+								rotation: 'down',
+							},
+						},
+					},
+				},
+				{
+					id: 'no',
+					name: 'No',
+					default: true,
+
+				},
+			],
+		},
+		breats_r: {
+			type: 'typed',
+			name: 'Attached to right breast',
+			variants: [
+				{
+					id: 'no',
+					name: 'No',
+					default: true,
+				},
+				{
+					id: 'yes',
+					name: 'Yes',
+				},
+			],
+		},
+		breats_l: {
+			type: 'typed',
+			name: 'Attached to left breast',
+			variants: [
+				{
+					id: 'no',
+					name: 'No',
+					default: true,
+				},
+				{
+					id: 'yes',
+					name: 'Yes',
+				},
+			],
+		},
+		fillLevel: {
+			type: 'typed',
+			name: 'Current fill level',
+			variants: [
+				{
+					id: 'empty',
+					name: 'Empty',
+					default: true,
+				},
+				{
+					id: 'slightly',
+					name: 'Slightly',
+				},
+				{
+					id: 'almost',
+					name: 'Almost full',
+				},
+				{
+					id: 'full',
+					name: 'Filled',
+				},
+			],
+		},
+	},
+	chat: {
+		actionAdd: 'SOURCE_CHARACTER put ITEM_ASSET_NAME into TARGET_CHARACTER_DYNAMIC_POSSESSIVE hand.',
+		actionRemove: 'SOURCE_CHARACTER removed ITEM_ASSET_NAME from TARGET_CHARACTER_DYNAMIC_POSSESSIVE hand.',
+	},
+	ownership: {
+		responsibleContributor: 'Sandrine <118102950+SandrinePDR@users.noreply.github.com>',
+		credits: ['Sandrine'],
+		modificationPolicy: `Fixes and New uses, otherwise ask`,
+		reusePolicy: 'Ask first',
+		licensing: [
+			{
+				part: 'lotion',
+				source: 'Self-Made',
+				copyrightHolder: 'Tom',
+				editedBy: 'Sandrine',
+				license: 'Pandora-Use-Only-NoModify-v1-or-later',
+			},
+		],
+	},
+});
