531b65fdcef6cdbf1f7c0de9729292fca31e1b5f
Sandrine <118102950+SandrinePDR@users.noreply.github.com>
2026-06-26 18:30:56 +0200

    [ADD] Hand pump (#627)

---
+++ src/assets/handhelds/handheld_breast_pump/handheld_breast_pump.asset.ts
@@ -0,0 +1,137 @@
+DefineAsset({
+	name: 'Breast Pump (handheld)',
+	size: 'small',
+	graphics: 'graphics.json',
+	colorization: {
+		handle: {
+			name: 'Handle',
+			default: '#C191D6',
+		},
+		junction: {
+			name: 'Junction',
+			default: '#C191D6',
+		},
+		topLid: {
+			name: 'Top Lid',
+			default: '#C191D6',
+		},
+		glass: {
+			name: 'Glass',
+			default: '#1BB6FF00',
+			minAlpha: 0.8,
+		},
+		bottomLid: {
+			name: 'Bottom Lid',
+			default: '#C191D6',
+		},
+		suctionCup: {
+			name: 'Suction Cup',
+			default: '#FFB570',
+			minAlpha: 0.2,
+		},
+		fluid: {
+			name: 'Fluid',
+			default: '#FFE599',
+		},
+	},
+	roomDeployment: {
+		autoDeployRelativePosition: [350, -50, 0],
+	},
+	// size:175, y:400, X:655
+	preview: 'preview.png',
+	attributes: {
+		provides: [
+			'Handheld',
+		],
+		requires: [
+			'!Hand_restricting_cover',
+		],
+	},
+	modules: {
+		handUsage_r: {
+			type: 'typed',
+			name: 'Held in right hand',
+			variants: [
+				{
+					id: 'yes',
+					name: 'Yes',
+					default: true,
+					properties: {
+						poseLimits: {
+							rightArm: {
+								fingers: 'fist',
+								rotation: 'down',
+							},
+						},
+					},
+				},
+				{
+					id: 'no',
+					name: 'No',
+				},
+			],
+		},
+		handUsage_l: {
+			type: 'typed',
+			name: 'Held in left hand',
+			variants: [
+				{
+					id: 'yes',
+					name: 'Yes',
+					properties: {
+						poseLimits: {
+							leftArm: {
+								fingers: 'fist',
+								rotation: 'down',
+							},
+						},
+					},
+				},
+				{
+					id: 'no',
+					name: 'No',
+					default: true,
+
+				},
+			],
+		},
+		fillLevel: {
+			type: 'typed',
+			name: 'Current fill level',
+			variants: [
+				{
+					id: 'empty',
+					name: 'Empty',
+					default: true,
+				},
+				{
+					id: 'slightly',
+					name: 'Slightly',
+				},
+				{
+					id: 'almost',
+					name: 'Almost full',
+				},
+				{
+					id: 'full',
+					name: 'Filled',
+				},
+			],
+		},
+	},
+	ownership: {
+		responsibleContributor: 'Sandrine <118102950+SandrinePDR@users.noreply.github.com>',
+		credits: ['Sandrine'],
+		modificationPolicy: `Fixes and New uses, otherwise ask`,
+		reusePolicy: 'Ask first',
+		licensing: [
+			{
+				part: 'pump',
+				source: 'Self-Made',
+				copyrightHolder: 'Tom',
+				editedBy: 'Sandrine',
+				license: 'Pandora-Use-Only-NoModify-v1-or-later',
+			},
+		],
+	},
+});
