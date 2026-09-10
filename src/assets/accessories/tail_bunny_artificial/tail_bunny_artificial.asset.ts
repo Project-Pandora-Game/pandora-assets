3a93c71ded80077bdc6aab1bc66bc17c930d5bd4
Nikky90506 <122885812+Nikky90506@users.noreply.github.com>
2026-09-10 19:34:26 +0300

    artificial tails from body part tails

---
+++ src/assets/accessories/tail_bunny_artificial/tail_bunny_artificial.asset.ts
@@ -0,0 +1,33 @@
+DefineAsset({
+	name: 'Artificial Bunny Tail',
+	size: 'small',
+	graphics: 'graphics.json',
+	colorization: {
+		tail: {
+			name: 'Tail',
+			default: '#EAEAEA',
+		},
+	},
+	//Size: 230, y: 620, centered
+	preview: 'preview.png',
+	attributes: {
+		provides: [
+			'Fantasy',
+			'Fantasy_tail',
+		],
+	},
+	ownership: {
+		responsibleContributor: 'Sandrine <118102950+SandrinePDR@users.noreply.github.com>',
+		credits: ['Taja'],
+		modificationPolicy: `Fixes and New uses, otherwise ask`,
+		reusePolicy: 'Ask first',
+		licensing: [
+			{
+				source: 'Self-Made',
+				copyrightHolder: 'Taja',
+				editedBy: 'Sandrine',
+				license: 'Pandora-Use-Only-v1-or-later',
+			},
+		],
+	},
+});
